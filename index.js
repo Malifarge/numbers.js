720072d8895b6568c128a43de916d696b21fa8b2
Thomas <malifarge.thomas9@gmail.com>
2022-06-20 16:48:43 +0200

    reste division euclidienne

--- index.js
+++ index.js
@@ -29,8 +29,51 @@ console.log(test/bis);
 console.log(test**bis);
 console.log(test%bis);
 
+// 05 - Reste division euclidienne
 
 
+let numberTest = 999
 
 
+for (let numberDiv = 1; numberDiv<6; numberDiv ++ ){
+if(numberTest%numberDiv == 0) {
+    console.log(`${numberTest} est divisible par ${numberDiv}`);
+    numberDiv +=1
+}
+}
 
+numberTest=25
+
+for (let numberDiv = 1; numberDiv<6; numberDiv ++ ){
+if(numberTest%numberDiv == 0) {
+    console.log(`${numberTest} est divisible par ${numberDiv}`);
+    numberDiv +=1
+}
+}
+
+numberTest = 190280
+
+for (let numberDiv = 1; numberDiv<6; numberDiv ++ ){
+    if(numberTest%numberDiv == 0) {
+        console.log(`${numberTest} est divisible par ${numberDiv}`);
+        numberDiv +=1
+    }
+    }
+
+    numberTest =47
+
+    for (let numberDiv = 1; numberDiv<6; numberDiv ++ ){
+        if(numberTest%numberDiv == 0) {
+            console.log(`${numberTest} est divisible par ${numberDiv}`);
+            numberDiv +=1
+        }
+        }
+
+    numberTest = 691
+
+    for (let numberDiv = 1; numberDiv<6; numberDiv ++ ){
+        if(numberTest%numberDiv == 0) {
+            console.log(`${numberTest} est divisible par ${numberDiv}`);
+            numberDiv +=1
+        }
+        }
